988e53434e02e0de4f93118aa9f6d38c3e2db5a4
Jeffrey <Jeffreystijnen0@gmail.com>
2022-03-09 15:30:25 +0100

    add 5.1.5

---
+++ 5.1.5.py
@@ -0,0 +1,23 @@
+#blink LED  /home/pi
+import RPi.GPIO as GPIO
+import time 
+# to use raspberry PI GPIO numbers
+GPIO.setmode(GPIO.BCM)   #GPTO18
+
+#blinking function
+def blink(pin):
+    #setup GPIO output channel
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 1)
+    time.sleep(1)
+    GPIO.output(pin, 0)
+    time.sleep(1)
+
+#main program blink GPIO18(pin12) 10 times
+for i in range(0,10):
+    blink(18) 
+    blink(23)
+
+#cleanup
+GPIO.cleanup()
+print("program executed")
