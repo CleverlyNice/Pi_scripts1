fb569e6fe79db02f36c65605bfb8b921df002a1c
Jeffrey <Jeffreystijnen0@gmail.com>
2022-03-23 14:25:38 +0100

    add 5.3.6.py

---
+++ 5.3.6.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time 
+import datetime
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup (17, GPIO.OUT)
+GPIO.setup (18, GPIO.IN)
+
+while True:
+
+    GPIO.output (17, 1)
+    time.sleep (0.00001)
+    GPIO.output (17, 0)
+    while (GPIO.input(18)==0):
+
+        pass
+    
+    
+    signalhigh = datetime.datetime.now()
+    
+    while (GPIO.input(18)==1):
+        pass
+
+    
+    signallow = datetime.datetime.now()
+
+    timepassed = signallow - signalhigh
+    distance = timepassed.total_seconds()*17000
+    
+    time.sleep(0.5)
+    if distance <= 30:
+        print("alarm")
+        print(distance)
+    else:
+        print("safe")
